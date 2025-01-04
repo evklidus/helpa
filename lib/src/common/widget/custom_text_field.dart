26fda1835f1563d4d8c30c9b6cd75eded2e51ef9
e.minasov <minasov@synaptik.ru>
2025-01-04 14:53:22 +0300

    Обновил зависимости и textField

--- lib/src/common/widget/custom_text_field.dart
+++ lib/src/common/widget/custom_text_field.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:helpa/src/common/utils/context_extension.dart';
 
 /// Custom-styled [TextFormField].
 class CustomTextField extends StatelessWidget {
@@ -94,55 +93,8 @@ class CustomTextField extends StatelessWidget {
       padding: EdgeInsets.symmetric(vertical: dense ? 8 : 2),
       child: TextFormField(
         style: style,
-        onTapOutside: onTapOutside,
-        enabled: enabled,
-        focusNode: focusNode,
-        validator: validator,
-        maxLength: maxLength,
         maxLines: maxLines,
         controller: controller,
-        textInputAction: textInputAction,
-        keyboardType: keyboardType,
-        inputFormatters: inputFormatters,
-        onChanged: onChanged,
-        decoration: decoration ??
-            InputDecoration(
-              labelStyle: labelStyle ??
-                  context.textTheme.bodyMedium!.copyWith(
-                    color: context.colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-              errorStyle: errorStyle,
-              hintText: hintText,
-              filled: true,
-              fillColor: context.colorScheme.background,
-              errorBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFFB95D6D)),
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-              ),
-              focusedErrorBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFFB95D6D)),
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-              ),
-              enabledBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFF272727)),
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-              ),
-              disabledBorder: const OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFF272727)),
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: context.colorScheme.primary.withOpacity(.4),
-                  width: 1.5,
-                ),
-                borderRadius: const BorderRadius.all(Radius.circular(16)),
-              ),
-              labelText: label,
-              isDense: true,
-              suffixIcon: suffix,
-            ),
       ),
     );
   }
